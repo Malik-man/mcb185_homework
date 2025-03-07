3541ef069164067cc731f3dff0cf986c9e2bfd1d
Malik-man <mnhassan@ucdavis.edu>
2025-03-06 19:40:05 -0800

    up

--- sequence.py
+++ sequence.py
@@ -12,16 +12,38 @@ def revcomp(dna):
 	return ''.join(rc)
 	
 def translate(dna):
-	aas = []
-	for i in range(0, len(dna), 3):
-	   codon = dna[i:i+3]
-	   if	codon == 'ATG': aas.append('M')
-	   elif codon == 'ATG': aas.append('*')
-	   elif codon == 'TAA': aas.append('*')
-	   elif codon == 'TAG': aas.append('*')
-	   elif codon == 'TGA': aas.append('*')
-	   else:				aas.append('*')
-	   return ''.join(aas)
+    codon_table = {
+        'ATA': 'I', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M',
+        'ACA': 'T', 'ACC': 'T', 'ACG': 'T', 'ACT': 'T',
+        'AAC': 'N', 'AAT': 'N', 'AAA': 'K', 'AAG': 'K',
+        'AGC': 'S', 'AGT': 'S', 'AGA': 'R', 'AGG': 'R',
+        'CTA': 'L', 'CTC': 'L', 'CTG': 'L', 'CTT': 'L',
+        'CCA': 'P', 'CCC': 'P', 'CCG': 'P', 'CCT': 'P',
+        'CAC': 'H', 'CAT': 'H', 'CAA': 'Q', 'CAG': 'Q',
+        'CGA': 'R', 'CGC': 'R', 'CGG': 'R', 'CGT': 'R',
+        'GTA': 'V', 'GTC': 'V', 'GTG': 'V', 'GTT': 'V',
+        'GCA': 'A', 'GCC': 'A', 'GCG': 'A', 'GCT': 'A',
+        'GAC': 'D', 'GAT': 'D', 'GAA': 'E', 'GAG': 'E',
+        'GGA': 'G', 'GGC': 'G', 'GGG': 'G', 'GGT': 'G',
+        'TCA': 'S', 'TCC': 'S', 'TCG': 'S', 'TCT': 'S',
+        'TTC': 'F', 'TTT': 'F', 'TTA': 'L', 'TTG': 'L',
+        'TAC': 'Y', 'TAT': 'Y', 'TAA': '*', 'TAG': '*',
+        'TGC': 'C', 'TGT': 'C', 'TGA': '*', 'TGG': 'W'
+    }
+
+    protein = []
+
+    for i in range(0, len(dna) - (len(dna) % 3), 3):
+        codon = dna[i:i+3]
+        
+        # Check for unknown bases (e.g., NNN, NGG)
+        if 'N' in codon or 'Y' in codon or 'R' in codon:
+            protein.append('?')  # '?' instead of 'N' to mark unknown codons
+        else:
+            protein.append(codon_table.get(codon, '?'))  # Default '?' for unknown codons
+
+    return ''.join(protein)
+
 
 def gc_comp(seq):
 	return (seq.count('C') + seq.count('G')) / len(seq)
@@ -33,12 +55,4 @@ def gc_skew(seq):
 	return (g - c) / (g + c)
 
 
-
-def gc_comp(seq):
-	return (seq.count('C') + seq.count('G')) / len(seq)
-	
-def gc_skew(g, c):
-    if g + c == 0:
-        return 0
-    return (g - c) / (g + c)
 
