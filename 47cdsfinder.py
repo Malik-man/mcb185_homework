3fa85c10f43050984fb888b24925f8366f924add
Malik-man <mnhassan@ucdavis.edu>
2025-03-06 19:50:30 -0800

    up

---
+++ 47cdsfinder.py
@@ -0,0 +1,19 @@
+import sys
+import sequence
+import mcb185
+
+s = sys.argv[1]
+
+def cdsfinder(fasta_file):
+	for defline, seq in mcb185.read_fasta(fasta_file):
+		seqs = seq.split('ATG')
+		seqs.pop(0)
+		for orf in seqs:
+			aa = sequence.translate(orf)
+			print('M', end = '')
+			if aa[-1] == '*': print(aa)
+			else: print(aa, end = '')
+
+print(cdsfinder(s))
+for defline, seq in mcb185.read_fasta(s):
+	print(cdsfinder(sequence.revcomp(seq)))
