38596919ff9db3eb3ab8dfd7620362d760ad4500
Malik-man <mnhassan@ucdavis.edu>
2025-03-06 18:47:36 -0800

    up

---
+++ test_sequence.py
@@ -0,0 +1,15 @@
+import sequence 
+
+print(sequence.transcribe('ACGT'))
+print(sequence.revcomp('AAAACGT'))
+print(sequence.translate('ATGCCCTAA')) 
+
+s = 'ACGTGGGGGGCATATG'
+print(sequence.gc_comp(s))
+print(sequence.gc_skew(s), sequence.gc_skew(sequence.revcomp(s)))
+
+seq = 'ACGTACGTGGGGGACGTACGTCCCCC'
+w = 10
+for i in range(len(seq) -w +1):
+	s = seq[i:i+w]
+	print(i, sequence.gc_comp(s), sequence.gc_skew(s))
